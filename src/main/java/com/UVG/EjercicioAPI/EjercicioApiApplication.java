4da7e25452d6cbbc9152c0d31f8187a86652c535
tismajo <mariajosegironisidro@gmail.com>
2025-03-27 21:18:18 -0600

    archives

---
+++ src/main/java/com/UVG/EjercicioAPI/EjercicioApiApplication.java
@@ -0,0 +1,13 @@
+package com.UVG.EjercicioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioApiApplication.class, args);
+	}
+
+}
